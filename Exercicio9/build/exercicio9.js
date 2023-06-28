20f8a5f86d2da5390cccd16aef11fc538b65d854
Tatiana02051 <tatianaserrano35044@gmail.com>
2023-06-27 21:50:57 -0300

    exercicios

---
+++ Exercicio9/build/exercicio9.js
@@ -0,0 +1,8 @@
+"use strict";
+function getParameterType(param1 = 456, param2 = 'Gremio', param3 = false) {
+    console.log(typeof (param1));
+    console.log(typeof (param2));
+    console.log(typeof (param3));
+}
+getParameterType();
+//# sourceMappingURL=exercicio9.js.map
---
+++ Exercicio9/src/exercicio9.ts
@@ -0,0 +1,6 @@
+function getParameterType(param1: number = 456, param2: string = 'Gremio', param3: boolean = false): void {
+    console.log(typeof(param1));
+    console.log(typeof(param2));
+    console.log(typeof(param3));
+}
+getParameterType();
